c53e33e628bc79606ae091d9034c82a5d6104f97
KerenLart <kklartey@st.ug.edu.gh>
2024-05-30 20:54:22 +0000

    made the template for the page

--- Task-app/App.js
+++ Task-app/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image,SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { FlatList, Image,SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+
+
 
 export default function App() {
   return (
@@ -22,12 +24,99 @@ export default function App() {
     </View>
     <View style={styles.searchtab}>
       <TextInput style={styles.search}> <Ionicons name="search" size={24}  />
-      <Text fontSize={16} fontWeight={700}>Search</Text></TextInput>
+      <Text style={{fontSize:16, fontWeight:700}}>Search</Text></TextInput>
       <Image 
           source={require('./assets/bx_slider.png')}
           style={styles.slider}
           />
     </View>
+    <Text style={{fontSize:20, fontWeight:"bold", marginTop:25}}>Categories</Text>
+    <ScrollView horizontal={true} >
+      <View style={styles.Categories}>
+        <View style={styles.task}>
+          <Text>Exercise</Text>
+          <Text>12 Tasks</Text>
+          <Image 
+            source={require('./assets/ladysitting.png')}
+            style={styles.taskimg}
+            />
+        </View>
+        <View style={styles.task}>
+          <Text>Exercise</Text>
+          <Text>12 Tasks</Text>
+          <Image 
+            source={require('./assets/ladysitting.png')}
+            style={styles.taskimg}
+            />
+        </View>
+        <View style={styles.task}>
+          <Text>Exercise</Text>
+          <Text>12 Tasks</Text>
+          <Image 
+            source={require('./assets/ladysitting.png')}
+            style={styles.taskimg}
+            />
+        </View>
+        <View style={styles.task}>
+          <Text>Exercise</Text>
+          <Text>12 Tasks</Text>
+          <Image 
+            source={require('./assets/ladysitting.png')}
+            style={styles.taskimg}
+            />
+        </View>
+        <View style={styles.task}>
+          <Text>Exercise</Text>
+          <Text>12 Tasks</Text>
+          <Image 
+            source={require('./assets/ladysitting.png')}
+            style={styles.taskimg}
+            />
+        </View>
+        <View style={styles.task}>
+          <Text>Exercise</Text>
+          <Text>12 Tasks</Text>
+          <Image 
+            source={require('./assets/ladysitting.png')}
+            style={styles.taskimg}
+            />
+        </View>
+        <View style={styles.task}>
+          <Text>Exercise</Text>
+          <Text>12 Tasks</Text>
+          <Image 
+            source={require('./assets/ladysitting.png')}
+            style={styles.taskimg}
+            />
+        </View>
+        <View style={styles.task}>
+          <Text>Exercise</Text>
+          <Text>12 Tasks</Text>
+          <Image 
+            source={require('./assets/ladysitting.png')}
+            style={styles.taskimg}
+            />
+        </View>
+      </View>
+    </ScrollView>
+    <Text>Ongoing Tasks</Text>
+    <FlatList styles={styles.flatlist}
+      data={[
+                { key : "1", text : "Dream Theater" }, 
+                { key : "2", text : "Enchant" },
+                { key : "3", text : "Fates Warning" }, 
+                { key : "4", text : "Kamelot" },
+                { key : "5", text : "Pyramaze" }, 
+                { key : "6", text : "Rush" },
+                { key : "7", text : "Serenity" }, 
+                { key : "8", text : "Shadow Gallery" },
+                { key : "9", text : "Pink Floyd" },
+                { key : "10", text : "Queensryche" }
+          ]}
+      renderItem={ ({item}) => <Text>{ item.text }</Text> }
+/>
+
+    
     </SafeAreaView>
     </View>
 
@@ -76,15 +165,35 @@ const styles = StyleSheet.create({
   },
   searchtab:{
     flexDirection: 'row',
-    display: 'flex',
-    justifyContent: '',
-    padding: 16,
+   
+    
+    
   },
   slider:{
-    width: 60,
-    height: 60,
+    width: 50,
+    height: 50,
     borderRadius: 10,
     backgroundColor:'orange',
     flex:1,
+  },
+
+  task:{
+    backgroundColor: '#fff',
+    borderRadius: 10,
+  },
+
+  taskimg:{
+    width:151,
+    height:132,
+  },
+
+  Categories:{
+    flexDirection: 'row',
+    padding: 16,
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  flatlist:{
+    
   }
 });
